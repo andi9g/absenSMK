ce35880e77ac25e10bf6fb3e8278842bd4d241f3
andi9g <andirizky.bayuputra@gmail.com>
2023-05-23 11:34:30 +0700

    perubahan

---
+++ public/masterUID/perangkat1.php
@@ -0,0 +1,8 @@
+<?php
+                            session_start();
+                            if($_SESSION['perangkat'] === 'perangkat1' && $_SESSION['computerId']==='6451f4bd06927'){
+                                    $UIDresult= '49E2926C';
+                                    echo $UIDresult; 
+                                }
+                            ?>
+                        
