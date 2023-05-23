7fca6209bf490574b254e532b4fe1f7554d342e1
andi9g <andirizky.bayuputra@gmail.com>
2023-05-23 11:31:20 +0700

    perubahan lama

--- resources/views/pages/pagesDesain.blade.php
+++ resources/views/pages/pagesDesain.blade.php
@@ -70,7 +70,7 @@
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Save</button>
+                        <button type="submit" class="btn btn-primary">Print</button>
                     </div>
                 </form>
                 </div>
