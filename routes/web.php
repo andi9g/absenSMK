b41189a8cd0fb41d6ed1e7dd71c7363c6ef1b86a
andi9g <andirizky.bayuputra@gmail.com>
2023-07-12 14:08:57 +0700

    oke

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('adminScan', 'scanC@adminscan');
 
 //post kartu
 Route::get('/', 'umumC@root');
-Route::get('/api/data/siswa', 'APIC@siswa');
+Route::get('/api/data/siswa.json', 'APIC@siswa');
 
 //login and logout
 Route::get('logout', 'aksesC@logout');
