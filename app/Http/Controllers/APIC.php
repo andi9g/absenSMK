8572c3e394454cacdb54056a02018b06c97e249d
andi9g <andirizky.bayuputra@gmail.com>
2023-07-17 11:22:13 +0700

    oke

--- app/Http/Controllers/APIC.php
+++ app/Http/Controllers/APIC.php
@@ -127,12 +127,12 @@ class APIC extends Controller
             $open = openM::first();
 
             foreach ($json as $key) {
-                $tanggal = date('Y-m-d', $key['waktu']);
-                $jam = date('H:i', $key['waktu']);
+                $tanggal = date('Y-m-d', $key->waktu);
+                $jam = date('H:i', $key->waktu);
 
                 $ambil = siswaM::join('card', 'card.nis', 'siswa.nis')
                 ->select('siswa.nis')
-                ->where('card.uid', $key['uid']);
+                ->where('card.uid', $key->uid);
 
                 if($ambil->count() == 1) {
                     $nis = $ambil->first()->nis;
