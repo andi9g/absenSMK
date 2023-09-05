57437fc22086f7c4e19dbb232bf0880e5060fd62
andi9g <andirizky.bayuputra@gmail.com>
2023-09-05 13:12:30 +0700

    oke

--- app/Http/Controllers/kenaikankelasC.php
+++ app/Http/Controllers/kenaikankelasC.php
@@ -73,7 +73,7 @@ class kenaikankelasC extends Controller
                 $idkelulusan = $tambah->idkelulusan;
                 $siswa = siswaM::where('idkelas', $kelas)->get();
                 foreach ($siswa as $s) {
-                    dd($s->namasiswa);
+                    dd($s->nis);
                     siswaM::where("nis", $s->nis)->update([
                         "idkelas" => $kelas2,
                         "idkelulusan" => $idkelulusan,
