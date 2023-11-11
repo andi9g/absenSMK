780a881d9222262415e99d9ff4d6566dbc7ce4a4
andi9g <andirizky.bayuputra@gmail.com>
2023-11-11 21:58:27 +0700

    oke

--- app/Http/Livewire/CekKartu.php
+++ app/Http/Livewire/CekKartu.php
@@ -17,7 +17,7 @@ class CekKartu extends Component
         $admin = adminM::where('id', $id)->first();
 
         $card = cardM::where('uid', $admin->value)->first();
-        $this->uid = empty($card->uid)?"tidak ditemukan":$card->uid;
+        $this->uid = empty($card->uid)?"tidak ditemukan":(String)$card->uid;
         $this->nis = empty($card->nis)?"tidak ditemukan":$card->nis;
         $this->namasiswa = empty($card->siswa->namasiswa)?"tidak ditemukan":$card->siswa->namasiswa;
         $this->ket = empty($card->ket)?"tidak ditemukan":$card->ket;
